8408c61a77714467181bc86fc9e60ddb14412f9e
ilia Pavlov <68978403+ilia-pavlov@users.noreply.github.com>
2023-08-28 23:30:56 -0400

    Initial Commit

---
+++ SwiftUIAccessibilityDemo/SwiftUIAccessibilityDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAccessibilityDemoApp.swift
+//  SwiftUIAccessibilityDemo
+//
+//  Created by Ilia Pavlov on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAccessibilityDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
